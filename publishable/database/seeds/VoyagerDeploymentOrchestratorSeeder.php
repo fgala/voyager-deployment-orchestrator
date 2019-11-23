60c590ede85785fef656dedf1f20794e9e1c2bb8
fgala <feg.fran@gmail.com>
2019-11-23 01:47:39 +0100

    Update VoyagerDeploymentOrchestratorSeeder.php
    I think is a typo, I got a problem reading the file.

--- publishable/database/seeds/VoyagerDeploymentOrchestratorSeeder.php
+++ publishable/database/seeds/VoyagerDeploymentOrchestratorSeeder.php
@@ -7,7 +7,7 @@ class VoyagerDeploymentOrchestratorSeeder extends Seeder
 {
     use Seedable;
 
-    protected $seedersPath = 'database/breads/seeds/';
+    protected $seedersPath = 'database/seeds/breads/';
 
     /**
      * Run the database seeds.
